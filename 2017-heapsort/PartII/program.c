b7b5561c2aef7be9e704de5d9a112c19d98e1308
Tom Cobley <tmc2118@ic.ac.uk>
2020-06-04 23:17:09 +0100

    Completed part II

--- 2017-heapsort/PartII/program.c
+++ 2017-heapsort/PartII/program.c
@@ -7,56 +7,59 @@ char *derived_lookup_table(char *);
 int contains(char *, char );
 
 int main(void) {
-  
-    char *s = "attack";
-  
-    printf("The initial word is: %s\n", s);
-  
-    char *table = derived_lookup_table(s);
-  
-    printf("Derived lookup table: %s\n\n", table);
-    
-    return EXIT_SUCCESS;
+
+  char *s = "attack";
+
+  printf("The initial word is: %s\n", s);
+
+  char *table = derived_lookup_table(s);
+
+  printf("Derived lookup table: %s\n\n", table);
+
+  return EXIT_SUCCESS;
 }
 
 char *derived_lookup_table(char *s) {
 
-	char * o_out = malloc(sizeof(s));
-	
-        if (o_out == NULL) {
-	    perror("malloc");
-	    exit(EXIT_FAILURE);
-	}
+  char * o_out = malloc(sizeof(s));
 
-	char * o_end = malloc(sizeof(s));
-          
-        o_end = o_out;
+  if (o_out == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
 
-        for ( ; *s != '\0' ; ++s) {
-	    if (!contains(o_out, *s)) {
-	      *o_end++ = *s;
-	    }
-	}
-   
-   	char *out = realloc(out, o_end - o_out + 1);
+  char * o_end = malloc(sizeof(s));
 
-	if (out == NULL) {
-	    perror("realloc");
-	    exit(EXIT_FAILURE);
-	  }
+  o_end = o_out;
 
-	return out;
+  for ( ; *s != '\0' ; ++s) {
+    if (!contains(o_out, *s)) {
+      *o_end++ = *s;
+    }
+  }
+
+
+  // CHANGE MADE: replace unitialised 'out' with o_out 
+  /* char *out = realloc(out, o_end - o_out + 1); */
+  char *out = realloc(o_out, o_end - o_out + 1);
+
+  if (out == NULL) {
+    perror("realloc");
+    exit(EXIT_FAILURE);
+  }
+
+  return out;
 
 }
 
 
 int contains(char *s, char c) {
-    
-    for (int i=0; i< strlen(s); i++){
-        if(s[i] == c){
-	   return 1;
-	}
+
+  for (int i=0; i< strlen(s); i++){
+    if(s[i] == c){
+      return 1;
     }
-    
-    return 0;
+  }
+
+  return 0;
 }
